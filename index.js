df6a10b7a3e1ac1cfb8edf14108c85e3856062af
Mahak-minfy <Mahak.yadav@minfytech.com>
2025-07-10 14:39:35 +0530

    commit

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ function add(a, b) {
     return a + b;
 }
 
-console.log(add(5, 3));
+// Export the function so other files (like your tests) can import it
+module.exports = add;
+
+// If you still want to log it, do it like this:
+if (require.main === module) {
+    console.log(add(5, 3));
+}
---
+++ test.js
@@ -0,0 +1,7 @@
+// __tests__/index.test.js
+
+const add = require('./index.js');
+
+test('adds 5 + 3 to equal 8', () => {
+    expect(add(5, 3)).toBe(8);
+});
